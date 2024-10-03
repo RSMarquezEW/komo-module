d8782958f535b17b96d0244aa591e723d5bbff99
RSMarquezEW <RSMarquez@eastwestbanker.com>
2024-10-03 23:52:51 +0800

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import ForgotPasswordComponent from "./ForgotPasswordComponent"
+import LoginComponent from "./LoginComponent"
+import RegisterComponent from "./RegisterComponent"
+export {ForgotPasswordComponent,LoginComponent,RegisterComponent}
